a291d3f3031f20c27a629dd0bf2a3f74d6fb72c3
Roxana Popa <sanica_97@yahoo.com>
2021-06-25 14:37:22 +0300

    Deploying organization setup

--- main.tf
+++ main.tf
@@ -1,17 +1,11 @@
+# Apelarea modulului aws-organization pentru a crea cele
+# trei conturi: prod, dev si audit
 module "setup_organization" {
-  source = "github.com/roxanapopa97/aws-organization?ref=v0.1"
+  source = "github.com/roxanapopa97/aws-organization?ref=v0.2"
   create_organization = true
-  organizational_units = {
-    "prod_organizational_unit" : {
-      "name"      : "prod",
-    },
-    "dev_organizational_unit" : {
-      "name"      : "dev",
-    },
-    "audit_organizational_unit" : {
-      "name"      : "audit",
-    }
-  }
+  organizational_units = [
+    "prod", "dev", "audit"
+  ]
   member_accounts = {
     "prod_account" : {
       "name"  : "prod",         
@@ -37,12 +31,20 @@ module "setup_organization" {
   }
 }
 
-# Extract audit account id
+
 locals {
+    # Extract account ids
     audit_account_id = coalesce([ for account in module.setup_organization.non_master_accounts[*]: account.name == "audit" ? account.id : null ])     
+    prod_account_id = coalesce([ for account in module.setup_organization.non_master_accounts[*]: account.name == "prod" ? account.id : null ])
+    dev_account_id = coalesce([ for account in module.setup_organization.non_master_accounts[*]: account.name == "dev" ? account.id : null ])
 }
 
+# Contul de audit trebuie sa devina delegated administrator
+# pentru a putea colecta informatiile din toata organizatia
 resource "aws_organizations_delegated_administrator" "audit" {
   account_id        = local.audit_account_id
   service_principal = "config.amazonaws.com"
 }
+
+# Este necesara crearea unui provider pentru fiecare cont
+# pentru a putea face assume role in fiecare account in parte
---
+++ outputs.tf
@@ -0,0 +1,11 @@
+output "dev_account_id" {
+  value = local.dev_account_id
+}
+
+output "prod_account_id" {
+  value = local.prod_account_id
+}
+
+output "audit_account_id" {
+  value = local.audit_account_id
+}
